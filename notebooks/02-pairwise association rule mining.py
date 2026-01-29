682419006cf00714c88ae8bed3d023c757250b59
Jongwook Eun <eunjongwook@gmail.com>
2026-01-29 17:55:30 +0900

    Delete notebooks/02-pairwise association rule mining.py

--- notebooks/02-pairwise association rule mining.py
+++
@@ -1,6 +0,0 @@
-# Association rule mining
-
-In this notebook, you'll implement the basic pairwise association rule mining algorithm.
-
-To keep the implementation simple, you will apply your implementation to a simplified dataset, namely, letters ("items") in words ("receipts" or "baskets"). Having finished that code, you will then apply that code to some grocery store market basket data. If you write the code well, it will not be difficult to reuse building blocks from the letter case in the basket data case.
-
